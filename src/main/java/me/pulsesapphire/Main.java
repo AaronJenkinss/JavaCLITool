f4dd9a83109b82fc6bd8601c610c3e93b9599af0
Aaron-Jenkins <aaronjenkins22442244@gmail.com>
2023-01-02 18:00:27 +0530

    Changed some code and fixed some bugs in the code.

--- src/main/java/me/pulsesapphire/Main.java
+++ src/main/java/me/pulsesapphire/Main.java
@@ -5,22 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Terminal terminal = new Terminal(true);
+
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
 
-        Terminal terminal = new Terminal(command, true);
-
-        while (true) {
+        while (terminal.isActive()) {
             String input = scanner.nextLine();
-            try {
-                if (terminal.isActive()) {
-                    terminal.sendInput(input);
-                } else {
-                    terminal = new Terminal(input, true);
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            terminal.sendInput(input);
         }
     }
 }
--- src/main/java/me/pulsesapphire/clipackage/Terminal.java
+++ src/main/java/me/pulsesapphire/clipackage/Terminal.java
@@ -2,6 +2,7 @@ package me.pulsesapphire.clipackage;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Terminal {
     private Thread thread;
@@ -14,52 +15,60 @@ public class Terminal {
 
     private BufferedWriter inputStream;
 
-    public Terminal(String command, boolean printToConsole) {
-        this.command = command;
+    public Terminal(boolean printToConsole) {
         this.isActive = true;
         this.printToConsole = printToConsole;
         this.output = new ArrayList<String>();
 
+        String operatingSystem = System.getProperty("os.name");
+        System.out.println(operatingSystem);
+
         try {
-            this.process = Runtime.getRuntime().exec(command);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            this.isActive = false;
-        }
+            if (operatingSystem.equals("Linux")) {
+                this.process = Runtime.getRuntime().exec("bash");
+            } else if (operatingSystem.equals("Windows 10")) {
+                this.process = Runtime.getRuntime().exec("cmd.exe");
+            } else {
+                throw new Exception("Operating system not supported.");
+            }
 
-        this.inputStream = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            this.inputStream = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
 
-        thread = new Thread(() -> {
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(this.process.getInputStream()));
+            thread = new Thread(() -> {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(this.process.getInputStream()));
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    this.output.add(line);
-                    if (this.printToConsole) System.out.println(line);
-                }
-
-                while (this.process.isAlive()) {
+                    String line;
                     while ((line = reader.readLine()) != null) {
                         this.output.add(line);
                         if (this.printToConsole) System.out.println(line);
                     }
 
-                    Thread.sleep(100);
+                    while (this.process.isAlive()) {
+                        while ((line = reader.readLine()) != null) {
+                            this.output.add(line);
+                            if (this.printToConsole) System.out.println(line);
+                        }
+
+                        Thread.sleep(100);
+                    }
+
+                    this.isActive = false;
+                    this.exitCode = process.waitFor();
+
+                    if (printToConsole) System.out.println("Process exited with code: " + this.exitCode);
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    this.isActive = false;
                 }
+            });
 
-                this.isActive = false;
-                this.exitCode = process.waitFor();
-
-                if (printToConsole) System.out.println("Process exited with code: " + this.exitCode);
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                this.isActive = false;
-            }
-        });
-
-        thread.start();
+            thread.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            this.isActive = false;
+        }
     }
 
     public void sendInput(String input) {
@@ -71,7 +80,23 @@ public class Terminal {
         }
     }
 
+    public boolean getPrintToConsole() {
+        return this.printToConsole;
+    }
+
+    public void setPrintToConsole(boolean printToConsole) {
+        this.printToConsole = printToConsole;
+    }
+
     public boolean isActive() {
-        return isActive;
+        return this.isActive;
+    }
+
+    public int getExitCode() {
+        return this.exitCode;
+    }
+
+    public ArrayList<String> getOutput() {
+        return this.output;
     }
 }
