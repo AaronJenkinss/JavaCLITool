517cd501621f653d268b10fec101e08c15a30301
Aaron-Jenkins <aaronjenkins22442244@gmail.com>
2023-01-03 14:48:56 +0530

    Changed some console logs.

--- src/main/java/me/pulsesapphire/Main.java
+++ src/main/java/me/pulsesapphire/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This CLI Tool written java is created by PulseSapphire. Contact @PulseSapphire#6734 on discord to talk to them.");
+        System.out.println("This CLI Tool is written in java and is created by PulseSapphire. Contact @PulseSapphire#6734 on discord to talk to them.");
         Terminal terminal = new Terminal(true, 100);
 
         Scanner scanner = new Scanner(System.in);
